a7e8bd1e56fdf76e358401666c490e64c1d223fc
Kishlay Koundinya <147274329+Kishlaykoundinya01@users.noreply.github.com>
2026-07-30 13:35:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true
+   // port: 5173,
+    //host: true
+    
+    allowedHosts: [
+      "campus-iq-ai-smart-campus-navigator.onrender.com"
+    ]
+  
   }
 })
